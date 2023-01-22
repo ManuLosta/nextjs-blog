7178eef50721a148a7ec3091d0c6228d220a1246
ManuLosta <mlostalom@gmail.com>
2023-01-21 21:01:43 -0300

    fixed errors

--- app/page.tsx
+++ app/page.tsx
@@ -9,6 +9,7 @@ export default function Home() {
         <h1 className="font-bold text-2xl text-slate-800 my-4">Latest Posts</h1>
         <PostGrid />
         <h1 className="mt-8 font-bold text-2xl text-slate-800 my-4">Repos</h1>
+        {/* @ts-expect-error Server Component */}
         <RepoList />
       </main>
     </PageWrapper>
